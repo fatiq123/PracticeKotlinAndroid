71c1c42c4830d54d1ac5184db39a6e86ae591d45
HP <fatiqhussnain1@gmail.com>
2023-06-19 00:49:56 -0700

    fragments with bottom navigation view

--- app/src/main/java/com/example/practice/MainActivity.kt
+++ app/src/main/java/com/example/practice/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.practice
 
 import android.content.Intent
 import android.content.SharedPreferences
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -9,46 +10,60 @@ import android.widget.EditText
 import android.widget.FrameLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var btnFragment1: Button
-    private lateinit var btnFragment2: Button
 
-
-
+    private val firstFragment = FirstFragment()
+    private val secondFragment = SecondFragment()
+    private val thirdFragment = ThirdFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnFragment1 = findViewById(R.id.btnFragment1)
-        btnFragment2 = findViewById(R.id.btnFragment2)
+        switchFragment(firstFragment)
 
-
-        val firstFragment = FirstFragment()
-        val secondFragment = SecondFragment()
-
-
-        supportFragmentManager.beginTransaction().apply{
-            replace(R.id.flFragment,firstFragment)
-            commit()
-        }
-
-        btnFragment1.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFragment,firstFragment)
-                addToBackStack(null)
-                commit()
+        val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener{
+            when (it.itemId){
+                R.id.miHome -> {
+                    switchFragment(firstFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.miMessage -> {
+                    switchFragment(secondFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
+                R.id.miProfile -> {
+                    switchFragment(thirdFragment)
+                    return@OnNavigationItemSelectedListener true
+                }
             }
+            false
         }
 
-        btnFragment2.setOnClickListener {
-            supportFragmentManager.beginTransaction().apply {
-                replace(R.id.flFragment,secondFragment)
-                addToBackStack(null)
-                commit()
-            }
-        }
+        val navigation: BottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
+
+        // to display badges on message icon
+        addBadges(navigation,R.id.miMessage,5)
+    }
+
+
+    private fun switchFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.flFragment, fragment)
+            .commit()
+    }
+
+    private fun addBadges(navigation: BottomNavigationView, itemId: Int, count: Int) {
+        val badge = navigation.getOrCreateBadge(itemId)
+        badge.number = count
+        badge.badgeTextColor = ContextCompat.getColor(this,R.color.badge_color)
+        badge.backgroundColor = ContextCompat.getColor(this,android.R.color.white)
+        badge.isVisible = true
     }
 }
---
+++ app/src/main/java/com/example/practice/ThirdFragment.kt
@@ -0,0 +1,7 @@
+package com.example.practice
+
+import androidx.fragment.app.Fragment
+
+class ThirdFragment : Fragment(R.layout.fragment_thrid) {
+
+}
